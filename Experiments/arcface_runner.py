ef9593db21cf62c82b062894db96af14ac2e94ab
Naitik Soni <soni.naitik>
2026-01-29 18:20:50 +0530

    Improved code for arcface runner for comparison

--- Experiments/arcface_runner.py
+++ Experiments/arcface_runner.py
@@ -1,33 +1,24 @@
-# from deepface import DeepFace
-# import numpy as np
-
-# # Returns a list of embeddings, faces, and locations
-# result = DeepFace.represent(img_path=r"P:\FaceTech\Face-Tech\Experiments\Aligned_image.png", model_name="ArcFace")
-
-# embedding = result[0]["embedding"]
-
-# em1 = np.array(embedding, dtype=np.float32)
-
-# em1 = em1 / np.linalg.norm(em1)
-
-# cosine_sim = np.dot(em1, em1)
-# print(cosine_sim)
-
 import onnxruntime as ort
 import numpy as np
 from face_preprocessing import normalize_face_input
 import cv2
 
-sess = ort.InferenceSession(r"P:\FaceTech\Face-Tech\models\arc.onnx",
+sess = ort.InferenceSession(r"..\models\arc.onnx",
                             providers=["CPUExecutionProvider"])
 
-img = cv2.imread("Aligned_image.png")
-x = normalize_face_input(img)
+def get_embedding(img_path):
+    img = cv2.imread(img_path)
+    x = normalize_face_input(img)
 
-input_name = sess.get_inputs()[0].name
-emb = sess.run(None, {input_name: x})[0]
+    input_name = sess.get_inputs()[0].name
+    emb = sess.run(None, {input_name: x})[0]
 
-# L2 normalize
-emb = emb / np.linalg.norm(emb, axis=1, keepdims=True)
+    # L2 normalize (CRITICAL)
+    emb = emb / np.linalg.norm(emb, axis=1, keepdims=True)
+    return emb
 
-print(emb.shape, np.linalg.norm(emb))
+emb1 = get_embedding("Aligned_image.png")
+emb2 = get_embedding("Aligned_image2.png")
+
+cosine_sim = np.dot(emb1, emb2.T)[0][0]
+print("Cosine similarity:", cosine_sim)
